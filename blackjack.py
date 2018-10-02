4cf4856765bdf3b59914d47e2395d1cb5e1febb4
hugowallstrom <43367159+hugowallstrom@users.noreply.github.com>
2018-10-02 11:11:41 +0200

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,17 @@
 import random
+fel = 0
 tal = (random.randint(1, 4))
-gissning = (int(input("Gissa om det är Hjärter, Spader, Klöver eller Rutter (1-4): ")))
+gissning = 0#start värden
 
-if gissning == tal:
-    print("Rätt.")
-else:
-    print("Fel. Rätt svar var:")
-    print(tal)
+while gissning != 5:
+        
+    gissning = (int(input("Gissa om det är Hjärter, Spader, Klöver eller Rutter (1-4): ")))
+    
+    if gissning == tal:
+        print("Rätt. Slumpar siffran igen. Antal fel: ")
+        print(fel)#visar felgissningar
+        tal = (random.randint(1, 4))
+        
+    else:
+        print("Fel. Försök igen.")
+        fel += 1#räknar antal fel
